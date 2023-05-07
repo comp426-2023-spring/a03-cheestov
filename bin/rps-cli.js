8c449e17071525121b7b319f3ed506bc8da702bf
Cat Tovey <cheestov@email.unc.edu>
2023-05-07 03:48:42 -0400

    added help text

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -6,3 +6,17 @@ import { rps } from "../lib/rpsls.js"
 
 var args = minimist(process.argv.slice(2));
 
+if (args.h || args.help) {
+    console.log('Usage: node-rps [SHOT');
+    console.log('Play Rock Paper Scissors (RPS)');
+    console.log();
+    console.log('  -h, --help      display this help message and exit');
+    console.log(' -r, --rules     display the rules and exit');
+    console.log()
+    console.log('Examples:');
+    console.log('  node-rps        Return JSON with single player RPS result.');
+    console.log('                  e.g. {"player":"rock"}');
+    console.log('  node-rps rock   Return JSON with results for RPS played against a simulated opponent.');
+    console.log('                  e.g {"player":"rock","opponent":"scissors","result":"win"}');
+    process.exit(0);
+}
