5bd15b6d9abb64d99a0d8c5bc70e4f275063467f
Cat Tovey <cheestov@email.unc.edu>
2023-05-07 04:05:37 -0400

    added help text

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -4,5 +4,19 @@ import minimist from 'minimist';
 import process from 'process';
 import { rpsls } from '../lib/rpsls.js';
 
-const argv = minimist(process.argv.slice(2));
+const args = minimist(process.argv.slice(2));
 
+if (args.h || args.help) {
+    console.log('Usage: node-RPSLS [SHOT]');
+    console.log("Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS!");
+    console.log();
+    console.log('  -h, --help        display this help message and exit');
+    console.log('  -r, --rules       display the rules and exit');
+    console.log();
+    console.log('Examples:');
+    console.log('  node-rpsls        Return JSON with single player RPSLS result.');
+    console.log('                    e.g. {"player":"rock"}');
+    console.log('  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.');
+    console.log('                    e.g {"player":"rock","opponent":"Spock","result":"lose"}');
+    process.exit(0);
+}
